393cc7d77facb10143a2ab666c2f98159b7e6417
duongxuanbinh <binhdx@newwave.vn>
2024-12-11 18:21:14 +0700

    test

--- starter/index.js
+++ starter/index.js
@@ -7,21 +7,21 @@ const replaceTemplate = require('./modules/replaceTemplate');
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //FILES
 // Blocking, synchronous way
-// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf8');
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf8');
 // console.log(textIn);
 // const textOut = `this is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./starter/txt/output.txt', textOut);
+// fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File written!');
 
 // Non-blocking, asynchronous way
-// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 //     if (err) return console.log('ERROR!');
-//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
 //         console.log(data2);
-//         fs.readFile(`./starter/txt/append.txt`, 'utf-8', (err, data3) => {
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
 //             console.log(data3);
 //
-//             fs.writeFile(`./starter/txt/final.txt`, `${data2}\n${data3}`, 'utf-8', err => {
+//             fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, 'utf-8', err => {
 //                 console.log('Your file has been written')
 //             })
 //         })
@@ -32,11 +32,11 @@ const replaceTemplate = require('./modules/replaceTemplate');
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //SERVER
 
-const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8')
-const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8')
-const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8')
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 
-const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {

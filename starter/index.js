6b03c809a89e0c0de01bfccb5f6d875b70fc27c7
duongxuanbinh <binhdx@newwave.vn>
2024-12-11 18:20:27 +0700

    test

--- starter/index.js
+++ starter/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const replaceTemplate = require('./starter/modules/replaceTemplate');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //FILES

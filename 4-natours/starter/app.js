1919ae8eb9a7b5026d220a101c0fed277999aad0
duongxuanbinh <binhdx@newwave.vn>
2025-01-01 21:26:26 +0700

    test

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRoute = require('./routes/tourRoutes');
 const userRoute = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -14,11 +16,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -26,5 +23,16 @@ app.use((req, res, next) => {
 // Route mounted
 app.use('/api/v1/tours', tourRoute);
 app.use('/api/v1/users', userRoute);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl}`,
+  // });
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
+});
+
+//Middleware for error handling (Auto recognized)
+app.use(globalErrorHandler);
 
 module.exports = app;

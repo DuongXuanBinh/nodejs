77609eaa3ebd775fc5d96c4ddf0cd977d69e1cfd
duongxuanbinh <binhdx@newwave.vn>
2025-01-18 22:57:36 +0700

    test

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -52,3 +52,25 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // check token exist
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(new AppError('You are not logged in'), 401);
+  }
+  // validate token
+
+  // check if user still exist
+
+  // check if user changed password after the JWT was issued
+  next();
+});
